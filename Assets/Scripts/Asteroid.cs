98b09888fb96a7e5703bbe9bfd6dbcd84774a458
poojapol01 <polpooja05@gmail.com>
2023-06-12 23:44:18 +0530

    Added Asteroid Script

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] sprites;
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    public float size = 1.0f;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        this.transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360.0f);
+        this.transform.localScale = Vector3.one * this.size;
+
+        _rigidbody.mass = this.size;
     }
 }
